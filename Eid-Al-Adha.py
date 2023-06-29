2a8d93960ca4ce0cbfe7de89f6ffc68ca44622d2
RABIUL ISLAM <120656583+Rabiulislam30@users.noreply.github.com>
2023-06-29 20:18:20 +0600

    Add files via upload

---
+++ Eid-Al-Adha.py
@@ -0,0 +1,10 @@
+def generate_eid_al_adha_mubarak_message():
+    message = """
+    Eid-AL-Adha Mubarak!
+    May the blessings of Allah fill your life with Joy, Prospertity, and Happiness!
+    May this Eid bring Peace, Love, and Unity to the world.
+    Wishing you and your Loved Ones a Blessed and Joynous Eid.
+    """
+
+    return message
+print(generate_eid_al_adha_mubarak_message())
